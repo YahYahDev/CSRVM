c5b9fa4c1c32a0c656a57cd6ac7d7e93fead18ce
YahYahDev <thomasbrianburkhardt@gmail.com>
2024-04-06 17:17:00 -0400

    finished arena module for now, testing needed.
    finished the arena module will need to test it next week.

--- src/Modules/Arena/Arena.c
+++ src/Modules/Arena/Arena.c
@@ -1,123 +1,218 @@
 #include "Arena.h"
-
-/*	TODO:
+#include <stdio.h>
+/* TODO:
  *
- *		1): Implement chained arenas
- *			that automaticly deallocate
- *			when your done with them
+ *  1): add a way to accese the last node in the linked list
+ *      without traversing the whole list
  *
  *
-*/
-
+ */
 
 typedef struct Arena{
 
+	// memory used to store data
 	void* data;
 
+	// ptr is used to index the data
+	size_t ptr;
+	// capacity is used to see what max capacity is
 	size_t capacity;
 
-	size_t filled;
 
-	size_t amount;
+	// these are used to store where the next node is for storage
+	Arena* next_node;
 
-	/* points to next node */
-	struct Arena* next;
+	Arena* head_node;
 
-	/* points to mother node */
-	struct Arena* header;
-
+	Arena* last_node;
 
 }Arena;
 
 
-/* Arena_Init()
+
+
+/* returns an Arena handle to use.
  *
- *	Initializes the values of the arena
- *	??kinda like a constructor??
  */
-Arena* Arena_Init(size_t size){
-	Arena* arena = _LibMemAlloc(sizeof(Arena));
+Arena* Arena_Init(size_t chunk_size){
 
-	arena->capacity = size;
-	arena->filled = 0;
-	arena->amount = 0;
+	// initalize the memory for the arena
+	Arena* arena = (Arena*)_LibMemAlloc(sizeof(Arena));
 
-	arena->next = NULL;
-	arena->header = NULL;
+	// get memory for storing object in the arena
+	arena->data = _LibMemAlloc(chunk_size);
 
-	arena->data = _LibMemAlloc(size);
+	arena->capacity = chunk_size;
 
+	arena->ptr = 0;
+
+	arena->head_node = NULL;
+
+	arena->next_node = NULL;
+
+	arena->last_node = arena;
 
 	return arena;
 }
 
 
-/*	Arena_Expand(Arena*, size_t);
- *
- *	Expands the capacity of an arena
- *
- *	If reallocation fails it will exit with code 1
- *
- *	??kinda like realloc??
-*/
-void Arena_Expand(Arena* arena, size_t size){
+/* used to add a arena to the list of arenas
+ * for allocations
+ */
+void Add_Arena(Arena* arena){
 
-	arena->capacity = size;
-	arena->data = _LibMemRealloc(arena->data, size);
+	Arena* new_arena = _LibMemAlloc(sizeof(Arena));
+	new_arena->capacity = arena->capacity;
+
+	new_arena->ptr = 0;
+
+	new_arena->data = _LibMemAlloc(new_arena->capacity);
+	new_arena->next_node = NULL;
+
+	new_arena->head_node = arena->last_node;
+
+	new_arena->last_node = NULL;
+
+	/* Set the old tail arena's next node to the new arena
+	 */
+	arena->last_node->next_node = new_arena;
+	/* Set the tail node to be the new arena
+	 */
+	arena->last_node = new_arena;
+
+}
+
+/* searches all arenas for a first fit to size
+ */
+void* Handle_Full_Arena(Arena* arena, size_t amount){
+	/* TODO: Note this might loop for ever
+	 * if Add_Arena() fails every time
+	 */
+	for(Arena* node = arena; node->next_node != NULL; node = node->next_node){
+		/* checks for if allocation will fit then allocates
+		 */
+		if(node->ptr + amount <= node->capacity){
+			node->ptr = node->ptr + amount;
+			return node->data + node->ptr - amount; // return allocation pointer;
+		}
+	}
+	return NULL; // failed at allocating???
+}
+
+/* used to clean up any linked arenas allocated
+ *
+ * returns the amount of arenas cleaned out
+ */
+size_t Arena_CleanUp(Arena* arena){
+
+	if(arena->last_node == arena) return 0;
+
+	size_t i = 0;
+	Arena* node = arena;
+	for(Arena* node = arena->last_node->head_node; node->head_node != NULL; node = node->head_node){
+		/* iterates through every node backwards to the head
+		 *
+		 *   frees next node in the list and NULL's it
+		 */
+		_LibMemFree(node->next_node->data);
+		node->next_node->data = NULL;
+
+		_LibMemFree(node->next_node);
+		node->next_node = NULL;
+
+		i++;
+	}
+
+	/* Frees the last node that was missed in the loop
+	 * and sets node->last to the arena for future allocations
+	 */
+	_LibMemFree(node->next_node->data);
+	node->next_node->data = NULL;
+
+
+	_LibMemFree(node->next_node);
+	node->next_node = NULL;
+
+
+	node->last_node = node;
+
+	i++;
+	/* returns i so that you can know how many arenas where free'd
+	 * for resizing the base arena when free'd
+	 */
+	return i;
+}
+
+
+/* used to resize the chunk for the memory
+ */
+void Arena_Expand(Arena* arena, size_t chunk_size){
+	arena->data = _LibMemRealloc(arena->data, chunk_size);
 
 	if(arena->data == NULL) exit(1);
 }
 
 
-/*	Arena_Free()
- *
- *	Frees/Nulls out any thing related to the arena
- *
- *	??kinda like a deconstructor??
-*/
-void Arena_Free(Arena* arena){
-	arena->capacity = 0;
-	arena->filled = 0;
-
-	/* Implement free all arenas linked */
-
+/* used to free the arena aka destroy it
+ */
+void* Arena_Free(Arena* arena){
+	Arena_CleanUp(arena);
+	if(arena->data == NULL || arena == NULL)
 	_LibMemFree(arena->data);
 	arena->data = NULL;
-
-
+	_LibMemFree(arena);
+	arena = NULL;
 }
 
 
 
 
-void* A_alloc(Arena* arena, size_t size){
-	/* checks if allocation would excede arena size
-	 * if it doesnt then it returns adress to the
-	 * memory and increases filled for the next allocation
-	 *
-	 * if it fails it returns NULL
-	*/
+void* A_alloc(Arena* arena, size_t amount){
 
-	if(arena->filled + size <= arena->capacity){
-		/* Checks first arena if it has space then
-		 * allocates from it if able
-		*/
-		size_t location = arena->filled;
-		arena->filled = arena->filled + size;
-		return arena->data + location;
+	size_t mem = 0;
+	if(arena->ptr + amount <= arena->capacity){
+		RETRY:
+
+		mem = arena->ptr;
+		arena->ptr = arena->ptr + amount;
+
+		return arena->data + mem;
+
+	}else if(amount > arena->capacity){
+		/* Handle Over Allocation */
+		Add_Arena(arena);
+		Arena_Expand(arena->last_node, amount);
+		perror("Failed to Allocate: Memory Chunk Too Small For Allocation");
+		return arena->last_node->data;
 	}else{
-		/* Returns NULL for failing */
-		return NULL;
-	}
+		/* Handle Chain Allocation */
+		Retry_Chain:;
 
-	/* TODO: add loging to arena allocation fails */
+		void* data = Handle_Full_Arena(arena, amount);
+
+		if(data != NULL){
+			return data;
+		}else{
+			Add_Arena(arena);
+			goto Retry_Chain;
+		}
+
+	}
+	return NULL;
 }
 
-
+/* deletes all data in the data
+ *
+ *   resizes arena size based on
+ *   how many arenas allocated before
+ *   freeing.
+ */
 void A_free(Arena* arena){
-	arena->filled = 0;
-	/* Implement free all arenas linked */
 
-	/* Implement expand arena based on how many linked arenas destroyed */
+	size_t regions = Arena_CleanUp(arena);
+
+	Arena_Expand(arena, arena->capacity * (size_t) regions * 0.75);
+
+	arena->ptr = 0;
 
 }
