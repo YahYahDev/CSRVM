2931a93a8de58e592e3ecb4486a65b3c20ab0377
YahYahDev <thomasbrianburkhardt@gmail.com>
2024-04-08 20:03:34 -0400

    Added instructions module
    Added instructions module for future use in the stack module

---
+++ src/Modules/Instructions/Instructions.h
@@ -0,0 +1,30 @@
+#ifndef Instructions_H
+#define Instructions_H
+
+#include <cstdint>
+
+/* Byte_Inst Aka Bye Instruction
+ *
+ *   enum to define what instructions their are
+ */
+typedef enum : uint8_t{
+	/* if a instruction = 0 then it unimplemented */
+	UNIMPLEMENTED = 0,
+
+
+}Byte_Inst;
+
+
+
+/* Byte_Op Aka Byte Operation
+ *
+ *   struct used to store instructions
+ *   and operands
+ */
+typedef struct {
+
+
+
+}Byte_Op;
+
+#endif
